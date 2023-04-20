85a641e78b845bd4a6f10dd5e1c2f5d60c913a70
vitalsen <vitalykazarin@yandex.ru>
2023-04-20 15:58:59 +0300

    1

--- IdeaProjects/newServlet/src/ru/appline/logic/Dataset.java
+++
@@ -1,9 +0,0 @@
-package ru.appline.logic;
-
-import java.io.Serializable;
-
-public class Dataset implements Serializable {
-    public String message;
-    public String code;
-    public double result;
-}
